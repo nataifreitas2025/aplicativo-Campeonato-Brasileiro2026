6b9a119df4594c068ea31987e4a35d2cee8d34ef
Natai Freitas Araujo <nataifreitas2025@gmail.com>
2026-06-25 21:33:17 -0300

    Em Andamento

---
+++ CampeonatoBrasileiro2026/ConsultarSerieCModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampeonatoBrasileiro2026
+{
+    internal class ConsultarSerieCModel
+    {
+    }
+}
